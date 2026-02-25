e3144fd5e5bdd7de1434f5319105e45438258fe9
308816 <UO308816@uniovi.es>
2026-02-25 11:45:09 +0100

    Error con el p2 de antes de entregar, borrado para solucionarlo

--- p2/rutalaboratorio.bat
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-set path=C:\Program Files\Java\jdk-24\bin
-set classpath=C:\Users\UO308816\Desktop\repositorio\alg_RaulGonzalezHeviaUO308816\p12
-=======
-set path=C:\Program Files\Java\jdk-22\bin
-set classpath=C:\Users\raulg\Desktop\p12
->>>>>>> 5895c88e9258b699d1868037c738edfd181e0da1
